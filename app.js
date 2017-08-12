87fc5001d619e7ef1b8f74aaa572120ac8e8d816
ayeshayaa <30959967+ayeshayaa@users.noreply.github.com>
2017-08-12 14:31:40 -0400

    changed image

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 console.log ('Vegetables are very healthy');
 alert ("Life is amazing");
 var vrHomePage = vRViewPlayer('#meiayeshadocs', {
-    image: 'mei.jpg',
+    image: 'IMG-2757.jpg',
     is_stereo: false,
     is_autopan_off: true,
 });
