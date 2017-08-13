0d8e4e50ebc33f765a8f7df7c3a22295553d2c38
ayeshayaa <30959967+ayeshayaa@users.noreply.github.com>
2017-08-13 00:15:50 -0400

    Update webVRDayTwo.js

--- webVRDayTwo.js
+++ webVRDayTwo.js
@@ -48,6 +48,22 @@ vrHomePage.on('click', function(event){
         image: 'IMG-2869.JPG',
         is_stereo: false
     });
+      vrHomePage.setContent({
+      image: 'IMG-2863.JPG',
+      preview: 'IMG-2863.JPG',
+      is_stereo: true
+    });
+        
+         vrHomePage.addHotspot('hotspot-back', {
+      pitch: 0,
+      yaw: 0,
+      radius: 0.10,
+      distance: 1
+    });
+        
+   if(event.id == 'hotspot-back') {
+    location.reload();
+  }
   }
     if (event.id == 'creating-hotspot-2'){
     vrHomePage.setContent({
@@ -64,21 +80,4 @@ vrHomePage.on('click', function(event){
     if (event.id == 'creating-hotspot-4'){
         theLareAlert("This is the Brooklyn Bridge Park Environmental Education Center!");
     }
-         vrHomePage.setContent({
-      image: 'IMG-2863.JPG',
-      preview: 'IMG-2863.JPG',
-      is_stereo: true
-    });
-        
-         vrHomePage.addHotspot('hotspot-back', {
-      pitch: 0,
-      yaw: 0,
-      radius: 0.10,
-      distance: 1
-    });
-        
-   if(event.id == 'hotspot-back') {
-    location.reload();
-  }
-    
-  });
+ });
