95d1d95b7c7bbb4ca3a81ab6c61f4b4d78cd3a2f
ayeshayaa <30959967+ayeshayaa@users.noreply.github.com>
2017-08-12 21:53:07 -0400

    Update webVRDayTwo.js

--- webVRDayTwo.js
+++ webVRDayTwo.js
@@ -12,24 +12,7 @@ vrHomePage.on('ready', function(events){
     radius: 0.10,
     distance: 1
   });
-    vrHomePage.addHotspot('creating-hotspot-2', {
-        pitch: -5,
-        yaw: 90,
-        radius: 0.10,
-        distance: 1
-    });
-    vrHomePage.addHotspot('creating-hotspot-3', {
-        pitch: -5,
-        yaw: 180,
-        radius: 0.10,
-        distance: 1
-    });
-    vrHomePage.addHotspot('creating-hotspot-4', {
-        pitch: -5,
-        yaw: -150,
-        radius: 0.10,
-        distance: 1
-    });
+    
     });
 
 vrHomePage.on('click', function(event){
@@ -50,38 +33,4 @@ vrHomePage.on('click', function(event){
   if(event.id == 'hotspot-back') {
     location.reload();
   } 
-
-vrHomePage.on('click', function(event){
-    console.log('register all click events');
-    if (event.id == 'creating-hotspot-2'){
-        vrHomePage.setContent({
-            image: 'IMG-2862.JPG',
-            is_stereo: false
-        });
-    }
-     vrHomePage.addHotspot('hotspot-back', {
-      pitch: 0,
-      yaw: 0,
-      radius: 0.10,
-      distance: 1
-    });
-  });
-
-  if(event.id == 'hotspot-back') {
-    location.reload();
-  }
-   
-    
-vrHomePage.on('click', function(event){
-    console.log('register all click events');
-    if (event.id == 'creating-hotspot-3'){
-        vrHomePage.setContent({
-            image: 'IMG-2861.JPG',
-            is_stereo: false
-        });
-    }
-    if(event.id == 'hotspot-back') {
-    location.reload();
-  }
-
 
