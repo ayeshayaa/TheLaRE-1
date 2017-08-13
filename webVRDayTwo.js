e527866e5b8fed8fcec72f99efc9590dc7318f8f
ayeshayaa <30959967+ayeshayaa@users.noreply.github.com>
2017-08-12 22:00:45 -0400

    Update webVRDayTwo.js

--- webVRDayTwo.js
+++ webVRDayTwo.js
@@ -23,14 +23,4 @@ vrHomePage.on('click', function(event){
         is_stereo: false
     });
   }
-     vrHomePage.addHotspot('hotspot-back', {
-      pitch: 0,
-      yaw: 0,
-      radius: 0.10,
-      distance: 1
-    });
-  })
-  if(event.id == 'hotspot-back') {
-    location.reload();
-  } 
-
+     
