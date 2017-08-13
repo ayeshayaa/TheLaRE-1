3f30bf0ba738766f977c6681954028a368ce49f7
ayeshayaa <30959967+ayeshayaa@users.noreply.github.com>
2017-08-12 22:44:47 -0400

    Update webVRDayTwo.js

--- webVRDayTwo.js
+++ webVRDayTwo.js
@@ -6,6 +6,7 @@ var vrHomePage = vRViewPlayer('#meiayeshadocs', {
 window.addEventListener('load', vrHomePage);
 
 vrHomePage.on('ready', function(events){
+    
   vrHomePage.addHotspot('creating-hotspot-1', {
     pitch: -5,
     yaw: -90,
@@ -17,19 +18,12 @@ vrHomePage.on('ready', function(events){
 
 vrHomePage.on('click', function(event){
   console.log('register all click events');
+    
   if (event.id == 'creating-hotspot-1'){
     vrHomePage.setContent({
         image: 'FullSizeRender%20(1).jpg',
         is_stereo: false
     });
   }
-    vrHomePage.addHotspot('hotspot-back', {
-      pitch: 0,
-      yaw: 0,
-      radius: 0.10,
-      distance: 1
-    });
-    if(event.id == 'hotspot-back') {
-    location.reload();
-  } 
+   
   });
